c5716519e38186ea3859d94a65a06bd175c89ba0
Scott Andrus <scott.andrus@vanderbilt.edu>
2012-12-04 22:03:16 -0600

    Added interface customization to profile vc

--- FaceLift/FLProfileViewController.h
+++ FaceLift/FLProfileViewController.h
@@ -15,8 +15,11 @@
 @property (strong, nonatomic) FLPerson *person;
 @property (strong, nonatomic) IBOutlet UIImageView *profilePictureImageView;
 @property (strong, nonatomic) IBOutlet UILabel *nameLabel;
-- (IBAction)matchFacePressed:(id)sender;
-- (IBAction)friendRequestPressed:(id)sender;
+@property (strong, nonatomic) IBOutlet UIView *backgroundView;
+
+@property (strong, nonatomic) IBOutlet UIButton *matchButton;
+@property (strong, nonatomic) IBOutlet UIButton *fbButton;
+
 - (IBAction)viewFBPressed:(id)sender;
 
 @end
--- FaceLift/FLProfileViewController.m
+++ FaceLift/FLProfileViewController.m
@@ -7,8 +7,10 @@
 //
 
 #import "FLProfileViewController.h"
-#import "SAViewManipulator.h"
+#import <QuartzCore/QuartzCore.h>
 #import "UIColor+i7HexColor.h"
+#import "UIView+Frame.h"
+#import "SAViewManipulator.h"
 
 @interface FLProfileViewController ()
 
@@ -32,6 +34,7 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
     [self populateUserInfo];
+    [self customizeUserInterface];
 }
 
 - (void)didReceiveMemoryWarning
@@ -43,13 +46,28 @@
 - (void)viewDidUnload {
     [self setProfilePictureImageView:nil];
     [self setNameLabel:nil];
+    [self setBackgroundView:nil];
+    [self setMatchButton:nil];
+    [self setFbButton:nil];
     [super viewDidUnload];
 }
 
 #pragma mark - Instance methods
 
 - (void)customizeUserInterface {
-    [SAViewManipulator setGradientBackgroundImageForView:self.view withTopColor:[UIColor colorWithHexString:@"F0F0F0"] andBottomColor:[UIColor colorWithHexString:@"C9C9C9"]];
+    [SAViewManipulator setGradientBackgroundImageForView:self.backgroundView withTopColor:[UIColor colorWithHexString:@"F0F0F0"] andBottomColor:[UIColor colorWithHexString:@"C9C9C9"]];
+    
+    // Customize buttons
+    [SAViewManipulator setGradientBackgroundImageForView:self.matchButton withTopColor:[UIColor colorWithHexString:@"FFA81C"] andBottomColor:[UIColor colorWithHexString:@"FF7B1C"]];
+    [SAViewManipulator addBorderToView:self.matchButton withWidth:.5 color:[UIColor blackColor] andRadius:10];
+    self.matchButton.clipsToBounds = YES;
+    
+    [SAViewManipulator setGradientBackgroundImageForView:self.fbButton withTopColor:[UIColor colorWithRed:0.11 green:0.188 blue:0.427 alpha:1] /*#1c306d*/ andBottomColor:[UIColor colorWithRed:0.231 green:0.349 blue:0.592 alpha:1] /*#3b5997*/];
+    [SAViewManipulator addBorderToView:self.fbButton withWidth:.5 color:[UIColor blackColor] andRadius:10];
+    self.fbButton.clipsToBounds = YES;
+    
+    [SAViewManipulator addBorderToView:self.profilePictureImageView withWidth:.5 color:[UIColor blackColor] andRadius:8];
+    
 }
 
 - (void)populateUserInfo {
@@ -63,16 +81,15 @@
     [self.presentingViewController dismissModalViewControllerAnimated:YES];
 }
 
-- (IBAction)matchFacePressed:(id)sender
-{
-}
-
-- (IBAction)friendRequestPressed:(id)sender {
-}
-
 - (IBAction)viewFBPressed:(id)sender
 {
-    NSURL *link = [NSURL URLWithString:[NSString stringWithFormat:@"http://facebook.com/%@", self.person.uid]];
+    NSURL *link = [NSURL URLWithString:[NSString stringWithFormat:@"fb://profile/%@", self.person.uid]];
+    if ([[UIApplication sharedApplication] canOpenURL:link]) {
+        [[UIApplication sharedApplication] openURL:link];
+    } else {
+        link = [NSURL URLWithString:[NSString stringWithFormat:@"http://facebook.com/%@", self.person.uid]];
+    }
+    
     [[UIApplication sharedApplication] openURL:link];
 }
 @end
